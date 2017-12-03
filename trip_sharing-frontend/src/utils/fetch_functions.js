a65f0fb054c404d6e273a0ac1c0b013aee33071c
Michal <michal.zurczak@gmail.com>
2017-12-03 14:22:40 +0100

    Create utils

---
+++ trip_sharing-frontend/src/utils/fetch_functions.js
@@ -0,0 +1,90 @@
+import { addToken, addUser, addTrips } from "../store/actions_creators"
+
+export const fetchSignIn = (user) => (dispatch) => {
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json'
+  });
+  
+  const config = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(user)
+  }
+  
+  return fetch('http://localhost:8080/api/users/sign_in', config)
+  .then(res => res.json())
+  .then(data => {
+    if ((data.token !== null ) && (data.token !== undefined)) {
+      const action = addToken(data);
+      dispatch(action);
+      localStorage.setItem('token', JSON.stringify(data.token))
+    }
+  })       
+}
+
+export const fetchNewUser = (user) => (dispatch) => {
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json'
+  });
+  
+  const config = {
+    method: "POST",
+    headers: myHeaders,
+    body: JSON.stringify(user)
+  }
+  return fetch('http://localhost:8080/api/users/sign_up', config)
+    .then(res => res.json())
+    .then(data => {
+      if ((data.token !== null )&& (data.token !== undefined)) {
+        const action = addToken(data);
+        dispatch(action);
+      }
+    })
+    .then(() => dispatch(fetchSignIn(user)));        
+}
+
+export const fetchUser = () => (dispatch) => {
+  const tokenJSON = localStorage.getItem('token');
+  const token = JSON.parse(tokenJSON);
+  if (token){
+    const myHeaders = new Headers({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${ token }`
+    });
+      
+    const config = {
+      method: "GET",
+      headers: myHeaders,
+    }
+
+    return fetch('http://localhost:8080/api/users/me', config)
+      .then(res  => res.json())
+      .then( data => {
+        const action = addUser(data);
+        dispatch(action)
+      })
+  }
+}
+
+export const fetchSearching = (searchItem) => (dispatch) => {
+  console.log(searchItem);
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json'
+  });
+
+  const config = {
+    method: "GET",
+    headers: myHeaders,
+  }
+
+  const url = `http://localhost:8080/api/trips/searchName?name=${searchItem}`
+  return fetch(url, config)
+    .then(res => res.json())
+    .then(data => {
+      const trips = {};
+      data.forEach( trip => {
+        trips[trip.id] = trip;
+      });
+      dispatch(addTrips(trips));
+  })
+}
