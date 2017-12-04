83b53502b2f47b400055178f6160b7d561971b87
Michal <michal.zurczak@gmail.com>
2017-12-04 15:57:52 +0100

    Trip's page coverphoto

---
+++ trip_sharing-frontend/src/components/Trip_CoverPhoto/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+import './index.css';
+
+class TripCoverPhoto extends Component {
+
+  render(){
+    return (
+      <div 
+        className = "TripCoverPhoto"
+        style = {
+          (this.props !== undefined ) 
+          ? { background: `url(${this.props.photoUrl}) no-repeat center center`,
+          height: "300px", backgroundSize: "contain" }
+          : {}
+      } >
+        <h1> { 
+          (this.props !== undefined) 
+          ? this.props.name 
+          : "" } 
+        </h1>
+        <h3> { 
+          (this.props.host !== undefined) 
+          ? `Hosted by ${ this.props.host} `
+          : "" } 
+        </h3>
+      </div>
+    )
+  }
+}
+export default TripCoverPhoto
