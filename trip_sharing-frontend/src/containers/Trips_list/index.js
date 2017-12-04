98b0866194dabb001d76f00f335b2990be4c1de8
Michal <michal.zurczak@gmail.com>
2017-12-04 16:01:01 +0100

    Trips list changes

---
+++ trip_sharing-frontend/src/containers/Trips_list/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import './index.css'
+
+import {GridList} from 'material-ui/GridList';
+
+import TripItem from '../../components/Trips_item'
+import { fetchAllTrips } from '../../utils/fetch_functions';
+
+class TripList extends Component {
+
+  componentWillMount() {
+    this.props.dispatch(fetchAllTrips());
+  }
+  render() {
+    return(
+      <div className = "TripList">
+        <h2>The newest</h2><br/>
+        <GridList
+          className = "TripList-gridList"
+          cellHeight = { 280 }
+          cols = { 3 }
+          padding = { 15 }
+        >
+          { (this.props.trips.length !== 0)
+            ? Object.values(this.props.trips).map((trip) => (
+            <TripItem trip = { trip } key = { trip.id }/>
+          ))
+          : "Loadign list of trips..."
+        }
+        </GridList>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ( { tripsReducer } ) => {
+  return ({
+    trips: tripsReducer.trips
+  })
+}
+
+export default connect(mapStateToProps)(TripList);
